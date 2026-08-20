442877ae4ef9e60a50683aae7221fd6ed86e14a3
Marcos Vinicius <marcos.viniciusl.porto06@gmail.com>
2026-08-20 15:59:38 -0300

    Inicio a o uso de brootstrap

---
+++ users/views.py
@@ -0,0 +1,34 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import logout, login, authenticate
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+
+def logout_view(request):
+    """Logout de usuario"""
+    logout(request)
+    return HttpResponseRedirect(reverse('learning_logs:index'))
+
+def register(request):
+    """Faz o cadastro de um novo usuário"""
+    if request.method != 'POST':
+        # Exibe um formulário em branco
+        form = UserCreationForm()
+
+    else:
+        # Processa o formulário preenchido
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # Faz login do usuário e redireciona para a página inicial
+            authenticated_user = authenticate(username=new_user.username,
+                                              password=request.POST['password1'])
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse('learning_logs:index'))  
+
+    context = {'form': form}
+    return render(request, 'users/register.html', context)      
+
+
